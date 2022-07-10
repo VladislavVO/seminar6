f9691faa1a8df4d6fad829933e4a2b75f4e7ea3d
Vladislav <vladislav.v.o@yandex.ru>
2022-07-10 21:06:27 +0300

    решена 2я задача

--- Program.cs
+++ Program.cs
@@ -7,3 +7,36 @@
 /*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
 */
+
+double[] crossPoint(double b1, double k1, double b2, double k2)
+{
+    double[] array = new double[2];
+
+    array[0] = ((b2 - b1) / (k1 - k2));
+    array[1] = (k2 * array[0]) + b2;
+
+    return array;
+}
+
+void ShowArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ";");
+
+    Console.WriteLine();    
+}
+
+Console.Write("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+double[] MyArray = crossPoint(b1, k1, b2, k2);
+Console.Write("Crosspoint of two lines: " );
+ShowArray(MyArray);
+
+
